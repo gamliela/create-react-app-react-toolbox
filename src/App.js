338fb7bb17a0b36744b15c45b4975f93a296a46d
gamliela <alon@gamliela.com>
2017-01-12 14:22:09 +0200

    added support in React Toolbox

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.scss';
+import { AppBar } from 'react-toolbox/lib/app_bar';
+import { Button } from 'react-toolbox/lib/button';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className={styles.app}>
+                <AppBar>That's app bar</AppBar>
+                <Button label="Hello World!" />
+                <div className={styles.appHeader}>
+                    <img src={logo} className={styles.appLogo} alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className={styles.appIntro}>
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
